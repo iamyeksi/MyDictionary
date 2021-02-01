4b665ed9025fae3f68be89d38d3ee23a3fc3e056
Yakup Kadri EKŞİ <56552125+iamyeksi@users.noreply.github.com>
2021-02-01 20:02:03 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/DictionaryList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class DictionaryList<tKey, tValue>
+    {
+        tKey[] keys;
+        tValue[] values;
+
+        public DictionaryList()
+        {
+            keys = new tKey[0];
+            values = new tValue[0];
+        }
+
+        public void Add(tKey key, tValue value)
+        {
+            tKey[] tempKey = keys;
+            tValue[] tempValue = values;
+
+            keys = new tKey[keys.Length + 1];
+            values = new tValue[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public tKey[] Keys
+        {
+            get { return keys; }
+        }
+        public tValue[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryList<int, string> names = new DictionaryList<int, string>();
+
+            names.Add(21, "Yakup Kadri");
+            names.Add(20, "Busesu");
+            names.Add(24, "Elif");
+
+
+            Console.WriteLine("Yaşı: " + names.Keys[0] + "\t" + "İsmi: " + names.Values[0]);
+            Console.WriteLine("Yaşı: " + names.Keys[1] + "\t" + "İsmi: " + names.Values[1]);
+            Console.WriteLine("Yaşı: " + names.Keys[2] + "\t" + "İsmi: " + names.Values[2]);
+
+
+
+        }
+    }
+}
